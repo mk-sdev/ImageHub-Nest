41c20b3f5350a1e32c8c3068f0dfbef427da17dc
Mikołaj <kaleta222@gmail.com>
2025-07-21 14:51:11 +0200

    configure mongodb

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,7 @@ import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { join } from 'path';
 import { PhotoModule } from './photo/photo.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
   imports: [
@@ -12,6 +13,7 @@ import { PhotoModule } from './photo/photo.module';
       driver: ApolloDriver,
       autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
     }),
+    MongooseModule.forRoot('mongodb://localhost:27017/imagehub'),
     PhotoModule,
   ],
   controllers: [AppController],
